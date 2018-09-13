6ed35dfd1674a2cfe37dc38539f97c3c6f5ba831
drili <DriliB@hotmail.com>
2018-09-13 22:09:38 +0200

    // save

---
+++ admin/includes/new_config.php
@@ -0,0 +1,7 @@
+<?php
+      // Database constants/connection
+      define('DB_HOST', 'localhost');
+      define('DB_USER', 'root');
+      define('DB_PASS', '');
+      define('DB_NAME', 'oop-gallery_db');
+?>
---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<?php include("includes/admin_header.php"); ?>
+
+      <!-- Navigation -->
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+            <?php include("includes/admin_navigation-top.php"); ?>
+
+            <?php include("includes/admin_navigation-sidebar.php") ?>
+      </nav>
+
+      <div id="page-wrapper">
+
+            <?php include("includes/admin_content.php") ?>
+
+      </div>
+      <!-- /#page-wrapper -->
+
+<?php include("includes/admin_footer.php"); ?>
